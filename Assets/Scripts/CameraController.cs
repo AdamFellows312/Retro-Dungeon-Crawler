e63f7013f78c779881295dfc210cc127a6f90759
SoftWet3445 <123949441+AdamFellows312@users.noreply.github.com>
2023-02-13 21:41:00 +0800

    Fixed jittery camera, implement camera zoom, improve some visual aspects

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,22 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class CameraController : MonoBehaviour
 {
-    private Transform target;
-
     private Vector3 velocity = Vector3.zero;
 
-    [SerializeField, Range(0.0f, 1.0f)] private float smoothTime = 0.3f;
+    private float targetZoom;
+    private float zoomSensitivity = 1.0f;
+    private float zoomTime = 30.0f;
+
+    private Camera targetCamera;
+
+    private Transform target;
+
+    [SerializeField] private float defaultZoom = 3.5f;
+    [SerializeField] private float maxZoom = 2.0f;
+    [SerializeField] private float minZoom = 5.0f;
     [Space(10)]
 
-    [SerializeField] private Vector3 targetFollowOffset; 
+    [SerializeField] private float smoothSpeed = 0.125f;
+    [Space(10)]
+
+    [SerializeField] Vector3 offset = new Vector3(0.0f, 0.5f, 0.0f);
 
     private void Start()
     {
-        // Get the inital target for the camera to follow
+        targetCamera = this.GetComponent<Camera>();
         target = GameObject.FindGameObjectWithTag("Player").transform;
+
+        targetZoom = defaultZoom;
+    }
+
+    private void Update()
+    {
+        CameraZoom();
     }
 
     private void LateUpdate()
@@ -24,12 +43,26 @@ public class CameraController : MonoBehaviour
         CameraFollow();
     }
 
+    private void CameraZoom()
+    {
+        // Get the target size based on the mouse scroll
+        targetZoom -= Input.mouseScrollDelta.y * zoomSensitivity;
+        // Clamp the target zoom between the max and minimum values
+        targetZoom = Mathf.Clamp(targetZoom, maxZoom, minZoom);
+
+        // Get the new size of the camera 
+        float newOrthoSize = Mathf.MoveTowards(targetCamera.orthographicSize, targetZoom, zoomTime * Time.deltaTime);
+        // Apply the new size to the camera
+        targetCamera.orthographicSize = newOrthoSize;
+    }
+
     private void CameraFollow()
     {
-        // Define a target position above and behind the target transform
-        Vector3 targetPosition = target.position + targetFollowOffset;
+        // Get and smooth the desired position
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.SmoothDamp(targetCamera.transform.position, desiredPosition, ref velocity, smoothSpeed);
 
-        // Smoothly move the camera towards that target position
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+        // Have the camera move towards the new position
+        targetCamera.transform.position = smoothedPosition;
     }
 }
